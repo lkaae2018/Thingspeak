659b619b3b83befdc97f5f2a18f0dba2f3a8cfea
lkaae2018 <lak@djhhadsten.dk>
2020-08-05 16:29:31 +0200

    Install

---
+++ Thingspeak_install.sh
@@ -0,0 +1,6 @@
+echo Dette script installere de nødvendige programmer til at kunne benytte Thingspeak!
+echo
+pip3 install thingspeak
+git clone http://github.com/mchwalisz/thingspeak.git
+export PATH=/home/pi/.loacl/bin:$PATH
+echo Nu er det muligt at lave programmer til Python3 samen med Thingspeak
